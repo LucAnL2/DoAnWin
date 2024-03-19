b4d12422f2720fac7a616ac3535f5173cc14ba89
LyHung10 <lyhung10nctlop95@gmail.com>
2024-03-19 08:20:07 +0700

    cha lam gi

--- DemoWin/Program.cs
+++ DemoWin/Program.cs
@@ -16,7 +16,7 @@ namespace DemoWin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FWorker());
         }
     }
 }
