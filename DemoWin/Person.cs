9c3a9804e5ce7713179f9cc6d7d19c4909dc9caf
LyHung10 <lyhung10nctlop95@gmail.com>
2024-03-20 14:56:59 +0700

    checknull

--- DemoWin/Person.cs
+++ DemoWin/Person.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace DemoWin
 {
@@ -62,5 +66,81 @@ namespace DemoWin
         public string Account { get => account; set => account = value; }
         public string Pass { get => pass; set => pass = value; }
         public string Role { get => role; set => role = value; }
+
+        public static bool checkNullDangKi(Person newPerson)
+        {
+            string[] propertiesToCheck = { "Name", "Email", "Account", "Pass", "Role" };
+
+            foreach (var propertyName in propertiesToCheck)
+            {
+                var property = newPerson.GetType().GetProperty(propertyName);
+                if (property == null) continue; // Kiểm tra xem thuộc tính có tồn tại không
+
+                if (property.GetValue(newPerson) == null || string.IsNullOrEmpty(property.GetValue(newPerson).ToString()))
+                {
+                    System.Windows.Forms.MessageBox.Show($"Thuộc tính {propertyName} không được để trống !!!");
+                    return false;
+                }
+                if (property.Name == "Account")
+                {
+                    string ac = property.GetValue(newPerson).ToString();
+                    if (!Regex.IsMatch(ac, "^[a-zA-Z0-9]{5,24}$"))
+                    {
+                        System.Windows.Forms.MessageBox.Show("Tên tài khoản có ít nhất 5 bao gồm chữ và số");
+                        return false;
+                    }
+                }
+                if (property.Name == "Email")
+                {
+                    string email = property.GetValue(newPerson).ToString();
+                    if (!Regex.IsMatch(email, @"^[a-zA-Z0-9_.]{3,20}@gmail.com(.vn|)$"))
+                    {
+                        System.Windows.Forms.MessageBox.Show("Email không hợp lệ !!!");
+                        return false;
+                    }
+                    else
+                    {
+                        var query1Result = ("SELECT * FROM NguoiDung2 WHERE Email = '" + email + "'");
+                        var query2Result = ("SELECT * FROM Worker2 WHERE Email = '" + email + "'");
+
+                        if (query1Result.Count() != 0 || query2Result.Count() != 0)
+                        {
+                            // Nếu có ít nhất một hàng được trả về từ bất kỳ truy vấn nào, tức là email đã tồn tại trong cơ sở dữ liệu
+                            System.Windows.Forms.MessageBox.Show("Email đã tồn tại trong cơ sở dữ liệu !!!");
+                            return false;
+                        }
+                    }
+                   
+                }
+            }
+            return true;
+        }
+
+       
+      
+        //private void btnRegister_Click(object sender, EventArgs e)
+        //{
+          
+        //    string confirmPassword = txtConfirmPassword.Text;
+        //    makeColor.ActivateButton(sender);
+        //    if (!CheckAccount(password)) { MessageBox.Show("Vui lòng nhập mật khẩu từ năm ký tự trở lên bao gồm của chữ và sô !!!"); return; }
+        //    if (confirmPassword != password) { MessageBox.Show("Vui lòng nhập chính xác mật khẩu !!!"); return; }
+        //    if (!CheckEmail(email)) { MessageBox.Show("Email không hợp lệ !!!"); return; }
+        //    if (modify.Accounts("Select * from Account where Email = '" + email + "'").Count != 0) { MessageBox.Show("Email này đã được sử dụng vui lòng dùng Email khác !!!"); return; }
+        //    try
+        //    {
+        //        string query = "Insert into Account values ('" + userName + "','" + password + "','" + email + "')";
+        //        modify.Command(query);
+        //        if (MessageBox.Show("Đăng ký thành công! Bạn có muốn đăng nhập luôn không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+        //        {
+        //            this.Close();
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        MessageBox.Show("Tài khoản này đã tồn tại !!!");
+        //    }
+        //}
     }
+    
 }
--- DemoWin/Worker.cs
+++ DemoWin/Worker.cs
@@ -9,13 +9,14 @@ namespace DemoWin
 {
     public class Worker : Person
     {
-        private string hireTimes;
         private string job;
         private string workingDay;
         private string workingTime;
         private string hireCost;
         private string experience;
         private string describe;
+        private string hireTimes;
+
 
         public Worker(string id, string name, string sex, string phone, string address, string cccd, string date, string email, string acNumber, string surplus) : base(id, name, sex, phone, address, cccd, date, email, acNumber, surplus)
         {
@@ -23,37 +24,65 @@ namespace DemoWin
         public Worker(string id, string job, string workingDay, string workingTime,
             string hireCost, string experience, string describe) : base(id)
         {
-            this.job = job;
-            this.workingDay = workingDay;
-            this.workingTime = workingTime;
-            this.hireCost = hireCost;
-            this.experience = experience;
-            this.describe = describe;
+            Job = job;
+            WorkingDay = workingDay;
+            WorkingTime = workingTime;
+            HireCost = hireCost;
+            Experience = experience;
+            Describe = describe;
         }
 
-        public string HireTimes { get => hireTimes; set => hireTimes = value; }
         public string Job { get => job; set => job = value; }
         public string WorkingDay { get => workingDay; set => workingDay = value; }
         public string WorkingTime { get => workingTime; set => workingTime = value; }
         public string HireCost { get => hireCost; set => hireCost = value; }
         public string Experience { get => experience; set => experience = value; }
         public string Describe { get => describe; set => describe = value; }
+        public string HireTimes { get => hireTimes; set => hireTimes = value; }
+
         public static class GlobalVariables
         {
             public static int Id = 3;
         }
+        //public static bool checkNullDangViec(Worker newDangviec)
+        //{
+
+        //    foreach (var property in newDangviec.GetType().GetProperties())
+        //    {
+        //        if (property.GetValue(newDangviec) == null)
+        //        {
+        //            MessageBox.Show($"Thuộc tính {property.Name} không được để trống !!!");
+        //            return false;
+        //        }
+        //    }
+        //    return true;
+        //}
         public static bool checkNullDangViec(Worker newWorker)
         {
-            foreach (var property in newWorker.GetType().GetProperties())
+            string[] propertiesToCheck = { "Id", "Job", "WorkingDay", "WorkingTime", "HireCost", "Experience", "Describe" };
+
+            foreach (var propertyName in propertiesToCheck)
             {
-                if (property.GetValue(newWorker) == null)
+                var property = newWorker.GetType().GetProperty(propertyName);
+                if (property == null) continue; // Kiểm tra xem thuộc tính có tồn tại không
+
+                if (property.GetValue(newWorker) == null || string.IsNullOrEmpty(property.GetValue(newWorker).ToString()))
                 {
-                    MessageBox.Show($"Thuộc tính {property.Name} không được để trống !!!");
+                    MessageBox.Show($"Thuộc tính {propertyName} không được để trống !!!");
                     return false;
+                }
+                if (property.Name == "Experience")
+                {
+                    int experienceValue;
+                    if (!int.TryParse(property.GetValue(newWorker).ToString(), out experienceValue))
+                    {
+                        MessageBox.Show($"Thuộc tính {property.Name} phải là một số nguyên !!!");
+                        return false;
+                    }
                 }
             }
             return true;
         }
-        
+
     }
 }
